dd7ef34714a4f3a03890da659ed9b330c99334fa
Shubham Chadokar <shubhamchadokar04@gmail.com>
2019-04-27 19:05:40 +0530

    react: web3 updated

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -12,12 +12,8 @@ if (process.env.NODE_ENV == "GANACHE" || process.env.NODE_ENV === undefined) {
   const eventProvider = new Web3.providers.WebsocketProvider(
     "ws://0.0.0.0:8545"
   );
-  if (typeof web3 !== "undefined") {
-    web3 = new Web3(web3.currentProvider);
-  } else {
-    // set the provider you want from Web3.providers
-    web3 = new Web3(new Web3.providers.HttpProvider("http://0.0.0.0:8545"));
-  }
+  web3 = new Web3(new Web3.providers.HttpProvider("http://0.0.0.0:8545"));
+  
   web3Network = "ganache";
   web3.setProvider(eventProvider);
 } else {
