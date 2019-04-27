328c2d4a04da50681979dd5bc05a04a76267bd9c
Shubham Chadokar <shubhamchadokar04@gmail.com>
2019-04-27 18:57:11 +0530

    react: ethereum module: server route modified

--- server/routes/contractAPI.js
+++ server/routes/contractAPI.js
@@ -4,13 +4,13 @@ const compile = require("../../ethereum/compile");
 const deploy = require("../../ethereum/deploy");
 
 router.post("/compile", async function(req, res, next) {
-    const result = compile();
-    res.send(result); 
+  const result = compile();
+  res.send(result);
 });
 
 router.post("/deploy", async function(req, res, next) {
-    const result = await deploy();
-    res.send(JSON.parse(result));
+  const result = await deploy();
+  res.send(JSON.parse(result));
 });
 
 module.exports = router;
--- server/routes/electionAPI.js
+++ server/routes/electionAPI.js
@@ -2,44 +2,146 @@ const express = require("express");
 const router = express.Router();
 const logic = require("../../ethereum/logic");
 
+// create a new election
+router.post("/newElection", async function(req, res, next) {
+  console.log(req.body);
+  const result = await logic.createElection(
+    req.body.account,
+    req.body.duration
+  );
+  res.send(result);
+  res.end("Election Created!");
+});
+
+// get all conducted election list
+router.get("/getElections", async function(req, res, next) {
+  console.log(req.body);
+  const result = await logic.getConductedElections();
+  res.send(result);
+  res.end("List of conducted elections!");
+});
+
+// get all conducted election list
+router.get("/getElections/:index", async function(req, res, next) {
+  console.log(req.params);
+  const result = await logic.getElectionAddress(parseInt(req.params.index));
+  res.send(result);
+  res.end("List of conducted elections!");
+});
+
+// get all conducted election list
+router.get("/getElectionAdmin/:address", async function(req, res, next) {
+  const result = await logic.getElectionAdmin(req.params.address);
+  res.send(result);
+  res.end("Admin of the Election!");
+});
+
+// add Consituency to Consituency list
+router.post("/addConsituency/:address", async function(req, res, next) {
+  console.log(req.body);
+  const address = req.params.address;
+  const { account, consituency } = req.body;
+  const result = await logic.addConsituency(account, address, consituency);
+  res.send(result);
+  res.end("Success");
+});
+
+// get consituency data from Consituency list
+router.get("/getConsituency/:address", async function(req, res, next) {
+  const result = await logic.getConsituency(
+    req.params.address,
+    req.body.consituency
+  );
+  res.send(result);
+});
+
 // add voter to voter list
-router.post("/addVoter", async function(req, res, next) {
-    console.log(req.body);
-    const {voterId, name, email, phoneNo, consituency, age} = req.body;
-    const result = await logic.addVoter(voterId, name, email, phoneNo, consituency, parseInt(age));
-    res.send(result);
-    res.end("Success");
+router.post("/addVoter/:address", async function(req, res, next) {
+  console.log(req.body);
+
+  const { account, voterId, name, email, phoneNo, consituency, age } = req.body;
+  const result = await logic.addVoter(
+    req.params.address,
+    account,
+    voterId,
+    name,
+    email,
+    phoneNo,
+    consituency,
+    parseInt(age)
+  );
+  res.send(result);
+  res.end("Success");
 });
 
 // get voter data from voter list
-router.get("/getVoter", async function(req, res, next) {
-    const result = await logic.getVoter(req.body.voterId);
-    res.send(result);
+router.get("/getVoter/:address", async function(req, res, next) {
+  const result = await logic.getVoter(req.params.address, req.body.voterId);
+  res.send(result);
 });
 
 // add candidate to election candidate list
-router.post("/addCandidate", async function(req, res, next) {
-    console.log(req.body);
-    const {candidateId, name, email, phoneNo, consituency} = req.body;
-    const result = await logic.addCandidate(candidateId, name, email, phoneNo, consituency);
-    res.send(result);
-    res.end("Success");
+router.post("/addCandidate/:address", async function(req, res, next) {
+  console.log(req.body);
+  const { account, candidateId, name, email, phoneNo, consituency } = req.body;
+  const result = await logic.addCandidate(
+    req.params.address,
+    account,
+    candidateId,
+    name,
+    email,
+    phoneNo,
+    consituency
+  );
+  res.send(result);
+  res.end("Success");
 });
 
 // get candidate data from candidate list
-router.get("/getCandidate", async function(req, res, next) {
-    const result = await logic.getCandidate(req.body.candidateId);
-    res.send(result);
+router.get("/getCandidate/:address", async function(req, res, next) {
+  const result = await logic.getCandidate(
+    req.params.address,
+    req.body.candidateId
+  );
+  res.send(result);
 });
 
-router.post("/castVote", async function(req, res, next) {
-    const result = await logic.castVote(req.body.candidateId);
-    console.log(result);
-    res.send(result);
+// cast the vote to candidate of respective consituency
+router.post("/castVote/:address", async function(req, res, next) {
+  const result = await logic.castVote(
+    req.params.address,
+    req.body.account,
+    req.body.candidateId
+  );
+  console.log(result);
+  res.send(result);
 });
 
-router.get("/getVotingCount", async function(req, res, next) {
-    const result = await logic.getVotingCount(req.body.consituency, req.body.candidateId);
-    res.send(result);
+// get voting count of a candidate of a respective consituency
+router.get("/getVotingCount/:address", async function(req, res, next) {
+  const result = await logic.getVotingCount(
+    req.params.address,
+    req.body.consituency,
+    req.body.candidateId
+  );
+  res.send(result);
+});
+
+// close the Election
+router.post("/closeElection/:address", async (req, res, next) => {
+  const result = await logic.closeElection(
+    req.params.address,
+    req.body.account
+  );
+  res.send(result);
+});
+
+// get the winner of the election
+router.get("/electionWinner/:address", async (req, res, next) => {
+  const result = await logic.winnerOfElection(
+    req.params.address,
+    req.body.account
+  );
+  res.send(result);
 });
 module.exports = router;
